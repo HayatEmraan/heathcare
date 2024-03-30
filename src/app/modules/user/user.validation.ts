0de6d7f5284e5fce25fcc3e090bb1933eeb16a19
hayatemraan <emraanhasanhayat@gmail.com>
2024-03-30 16:47:09 +0600

    refactor(prisma): doctor table refactored

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -5,10 +5,37 @@ const create = z.object({
     password: z.string(),
     role: z.string().optional(),
     email: z.string(),
-    admin: z.object({
-      name: z.string(),
-      contactNumber: z.string(),
-    }),
+    admin: z
+      .object({
+        name: z.string(),
+        contactNumber: z.string(),
+        photoURL: z.string().optional(),
+      })
+      .optional(),
+    patient: z
+      .object({
+        name: z.string(),
+        contactNumber: z.string(),
+        address: z.string(),
+        photoURL: z.string().optional(),
+      })
+      .optional(),
+    doctor: z
+      .object({
+        name: z.string(),
+        photoURL: z.string().optional(),
+        contactNumber: z.string(),
+        address: z.string(),
+        registrationNumber: z.string(),
+        experience: z.number(),
+        gender: z.string(),
+        appointmentFee: z.number(),
+        qualification: z.string(),
+        currentWorkingPlace: z.string(),
+        designation: z.string(),
+        averageRating: z.number(),
+      })
+      .optional(),
   }),
 });
 
